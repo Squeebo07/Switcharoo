66c4c321c2d1a677d2e740e1673b555c5f7d4901
MSI\Wu <zhangwu@sheridancollege.ca>
2019-04-25 12:14:20 -0400

    Odin alone test

---
+++ SwitcharooUnityProject/Switcheroo/Assets/RootGame/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button]
+    void SampleInvoke()
+    {
+        Debug.Log("Invoked" + gameObject.name);
+    }
+}
